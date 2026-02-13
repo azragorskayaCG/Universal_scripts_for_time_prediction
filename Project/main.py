5c98efde7fda9f97b7c0ca360183e6f706dcb00e
Clemence Azra-Gorskaya <157042528+azragorskayaCG@users.noreply.github.com>
2026-02-13 20:42:19 +0300

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,5 @@
 from pred_ML import *
 
-
 """Set these parameters before launching the code"""
 
 dirname = '/Users/clemence/Documents/Аспирантура_наука/1. Работа/2. Нейронные сети/g11-15_solar_cycle24/data/'
@@ -12,8 +11,11 @@ feature_names = ['kp', 'V',  'symh', 'Bz', 'P', '08MeV', 'ionT', 'Bt', 'n', 'Rss
 target = '2MeV'
 
 ahead = 3
+
 offset = 2
+
 size_split_valtest = 0.2
+
 size_split_test = 0.5
 
 # =========== 
